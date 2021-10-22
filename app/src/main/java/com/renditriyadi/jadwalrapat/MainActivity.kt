60ded9aa80fd68c66a9998c0c7d5a9203ecac055
mrtriyadi <mr.triyadi@gmail.com>
2021-10-22 21:25:20 +0700

    jadwal rapat. first commit

--- app/src/main/java/com/renditriyadi/jadwalrapat/MainActivity.kt
+++ app/src/main/java/com/renditriyadi/jadwalrapat/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.renditriyadi.jadwalrapat
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.renditriyadi.jadwalrapat.databinding.LayoutCustomDialogBinding
 
 class MainActivity : AppCompatActivity() {
+    private val dialogBinding : LayoutCustomDialogBinding
 
     private val rvDaftarRapat: RecyclerView by lazy {
         findViewById(R.id.rv_list_rapat)
@@ -44,6 +48,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openDialogTambah() {
+        dialogBinding = LayoutCustomDialogBinding.inflate(layoutInflater)
+        val dialog = AlertDialog.Builder(this)
+            .apply {
+                setView(dialogBinding.root)
+            }
+            .create()
 
+        if (pemenang=="SERI"){
+            dialogBinding.tvMenang.isVisible=false
+            dialogBinding.tvPemenang.text="SERI"
+        }else{
+            dialogBinding.tvMenang.isVisible=true
+            dialogBinding.tvPemenang.text=pemenang
+        }
+        dialogBinding.btnMainLagi.setOnClickListener{
+            dialog.dismiss()
+            clearChoice()
+        }
+        dialogBinding.btnMenu.setOnClickListener{
+            val intent= Intent(this, Menu::class.java)
+            intent.putExtra(Menu.NAME,playerName)
+            startActivity(intent)
+        }
+        dialog.show()
     }
 }
